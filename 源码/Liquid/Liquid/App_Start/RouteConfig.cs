d30748f38a1204a787d43df9acb8a2fc2f75e1fc
RukioRuler <626386490@qq.com>
2020-06-27 15:47:58 +0800

    1

--- 源码/Liquid/Liquid/App_Start/RouteConfig.cs
+++ 源码/Liquid/Liquid/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Liquid
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Mainpage", action = "Mainpage", id = UrlParameter.Optional }
             );
         }
     }
